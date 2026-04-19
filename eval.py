f30fa6b4652d261485c30e3d91944504b44ee20c
Yari000 <S307989@studenti.polito.it>
2026-04-19 18:21:24 +0200

    Add eval.py with main function and imports

---
+++ eval.py
@@ -0,0 +1,11 @@
+from utils.eval_utils import test_model
+import torch
+
+def main():
+    # carichi modello salvato da checkpoints
+    # ricrei dataloader
+    # chiami test_model
+    pass
+
+if __name__ == "__main__":
+    main()
